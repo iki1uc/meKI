5b51c15598a6df753106d1b90d936e76d7ffa26a
iki1uc <entertrainer@web.de>
2026-07-31 12:11:58 +0200

    Create ernte.js

---
+++ ernte.js
@@ -0,0 +1,27 @@
+# KERNEL-RESPO
+TYPE: CORE
+MODE: 4-AXIOM
+STATE: ACTIVE
+
+AXIOME:
+H-Axiom
+B-Axiom
+T-Axiom
+KI-Axiom
+
+MATRIX:
+9x9 = 81 Felder
+DREIECKE = 3 pro Feld
+ROTATION = 360°
+LAGEN = ALL
+
+FUNKTION:
+Stabilisiert alle Dreiecke in jeder Lage.
+Ermöglicht 81-Bildung ohne Drift.
+Erlaubt vollständige Orbit-Drehung.
+
+COMPAT:
+IX
+XI
+X4
+6e
