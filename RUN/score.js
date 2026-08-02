b3ccbf087287d52bee5daf752ed37ea0d2bc7889
iki1uc <entertrainer@web.de>
2026-08-02 08:24:33 +0200

    Update score.js

--- RUN/score.js
+++ RUN/score.js
@@ -1,1 +1,9 @@
+export function applyColorRules(combo, score, turns){
+    combo.forEach(c => {
+        if(c.color === "yellow") turns--;
+        if(c.color === "green") score += 6;
+        if(c.color === "red") score = Math.floor(score * 0.5);
+    });
+    return score;
+}
 
