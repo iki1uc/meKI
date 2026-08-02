2cc7a7be8c29dcc97a4eb3dbab91dff7dee4a251
iki1uc <entertrainer@web.de>
2026-08-02 08:55:33 +0200

    Update meKI.js

--- meKI.js
+++ meKI.js
@@ -7,7 +7,7 @@ import { MIATribunal } from "./mia-tribunal.js";
 import { orbitGate } from "./gates.js";
 
 export function meKI(currentRoom, tick, turns, axiomMode, slide){
- 
+
     const tech = techPulse(tick);
     const pulse = stabiliseRoom(currentRoom);
     const reality = RealityPulse(currentRoom, tick);
