c3c9db5fee5c8151d32c75a56ce267a140514dd4
iki1uc <entertrainer@web.de>
2026-08-02 08:50:34 +0200

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -6,9 +6,10 @@ export function visualize756(rooms){
 
     const result = [];
 
-    for(let tick = 0; tick < 84; tick++){        // 84 = LCM(9,11,4)
+    for(let tick = 0; tick < 84; tick++){
         for(let turns = 0; turns < 9; turns++){
             for(let slide = 0; slide < 3; slide++){
+
                 const axiomMode = ["IN","OUT","IO"][slide];
 
                 const calc = meKI(
