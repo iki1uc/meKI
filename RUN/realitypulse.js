af2c4fee689a1a2e95bc14ff2d85d544245559fe
iki1uc <entertrainer@web.de>
2026-08-02 08:25:18 +0200

    Update realitypulse.js

--- RUN/realitypulse.js
+++ RUN/realitypulse.js
@@ -1,1 +1,10 @@
+export function RealityPulse(room, tick){
+    return {
+        meta: {
+            mode: (tick % 2 === 0) ? "ALL-IN" : "ALL-OUT",
+            tick
+        },
+        room
+    };
+}
 
